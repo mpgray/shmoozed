749aabc3e8092280ef376fbd6813c5057d3d425d
Michael <michaelgray2@mail.weber.edu>
2018-10-05 13:36:16 -0600

    creating skeleton app components. Account, buying, dashboard, selling

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import {MatIconModule} from '@angular/material/icon';
+import { AccountComponent } from './account/account.component';
+import { SellingComponent } from './selling/selling.component';
+import { BuyingComponent } from './buying/buying.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    SellingComponent,
+    BuyingComponent,
+    DashboardComponent
   ],
   imports: [
     AlertModule.forRoot(),
