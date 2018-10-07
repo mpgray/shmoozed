8c9f6e4afae60e9816c00106091333407a46788b
Michael <michaelgray2@mail.weber.edu>
2018-10-06 19:11:21 -0600

    Route module editing

--- FrontEnd/src/app/app.routing.module.ts
+++ FrontEnd/src/app/app.routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [{
-  path: '',
+  path: 'dashboard',
   redirectTo: '/dashboard',
   pathMatch: 'full'
-},
+}, {
+    children: [{
+      path: 'dashboard',
+      component: DashboardComponent
+    }], },
   {
   path: '**',
   redirectTo: 'session/404'
