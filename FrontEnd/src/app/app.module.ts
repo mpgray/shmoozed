b6504ecb775859cb9d43c67d6bb2ceb0e4530a1e
Michael <michaelgray2@mail.weber.edu>
2018-10-08 18:26:02 -0600

    finished routes

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import {MatIconModule} from '@angular/material/icon';
+
 import { AccountComponent } from './account/account.component';
 import { SellingComponent } from './selling/selling.component';
 import { BuyingComponent } from './buying/buying.component';
--- FrontEnd/src/app/app.routing.module.ts
+++ FrontEnd/src/app/app.routing.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {BuyingComponent} from './buying/buying.component';
+import {SellingComponent} from './selling/selling.component';
+import {AccountComponent} from './account/account.component';
 
 const routes: Routes = [{
-  path: 'dashboard',
-  redirectTo: '/dashboard',
+  path: '',
+  redirectTo: 'dashboard',
   pathMatch: 'full'
 }, {
+  path: 'dashboard',
     children: [{
-      path: 'dashboard',
+      path: '',
       component: DashboardComponent
     }], },
+  {
+    path: 'account',
+    component: AccountComponent
+  }, {
+    path: 'buying',
+    component: BuyingComponent
+  }, {
+    path: 'selling',
+    component: SellingComponent
+  },
   {
   path: '**',
   redirectTo: 'session/404'
