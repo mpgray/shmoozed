20d3f28a18adf76ae1ea48c18b1de46d3dab35a5
Christopher Nash <christopher@nash-family.info>
2018-10-09 19:48:23 -0600

    Allow CORS Requests
    We found that Angular isn't allowed to call the endpoint because of a CORS issue. I've added a `@CrossOrigin` annotation which should allow this.

--- BackEnd/shmoozed/src/main/java/com/shmoozed/controller/ExampleController.java
+++ BackEnd/shmoozed/src/main/java/com/shmoozed/controller/ExampleController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin // Allow All CORS Requests. See https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
 @RestController
 @RequestMapping(path = "/example")
 public class ExampleController {
