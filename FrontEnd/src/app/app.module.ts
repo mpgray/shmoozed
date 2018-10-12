3b54c20bf860d4720a32c28629fa379a28f47a7a
Michael <michaelgray2@mail.weber.edu>
2018-10-11 20:01:15 -0600

    Initial REST service

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import {MatIconModule} from '@angular/material/icon';
+import {AppRoutingModule} from './app.routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AccountComponent } from './account/account.component';
 import { SellingComponent } from './selling/selling.component';
 import { BuyingComponent } from './buying/buying.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {AppRoutingModule} from './app.routing.module';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
