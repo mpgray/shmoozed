236d96b81767e69b12ef416abd5d2de477144dc6
Christopher Nash <christopher@nash-family.info>
2018-10-27 16:22:02 -0600

    Create Models for DB Entities
    These are based off the initial schema provided by the DBAs.

---
+++ BackEnd/shmoozed/src/main/java/com/shmoozed/model/SellerItem.java
@@ -0,0 +1,85 @@
+package com.shmoozed.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+/**
+ * Represents a set of items which a Seller is looking to sell.
+ */
+@Entity
+@Table(name = "Seller_Items")
+public class SellerItem {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "Seller_Item_Id")
+  private int id;
+
+  @Column(name = "Item_Id")
+  private int itemId;
+
+  @Column(name = "Price")
+  private BigDecimal price;
+
+  @Column(name = "User_Id")
+  private int userId;
+
+  public SellerItem() {
+    // Empty default constructor. This is needed in order for JPA to work properly.
+  }
+
+  public SellerItem(int id, int itemId, BigDecimal price, int userId) {
+    this.id = id;
+    this.itemId = itemId;
+    this.price = price;
+    this.userId = userId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getItemId() {
+    return itemId;
+  }
+
+  public void setItemId(int itemId) {
+    this.itemId = itemId;
+  }
+
+  public BigDecimal getPrice() {
+    return price;
+  }
+
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public void setUserId(int userId) {
+    this.userId = userId;
+  }
+
+  @Override
+  public String toString() {
+    return "SellerItem{" +
+      "id=" + id +
+      ", itemId=" + itemId +
+      ", price=" + price +
+      ", userId=" + userId +
+      '}';
+  }
+}
