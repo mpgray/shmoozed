a48d239c1aae4cbd29ab62fb68b24d00df3daeeb
Michael <michaelgray2@mail.weber.edu>
2018-10-30 16:33:12 -0600

    Renamed chart values to something a user might see

--- FrontEnd/src/app/shared/chart/chart.component.ts
+++ FrontEnd/src/app/shared/chart/chart.component.ts
@@ -9,11 +9,11 @@ export class ChartComponent {
 
   // lineChart
   public lineChartData: Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Nike Shoes'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Antique Doll'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Cat Toy Box'}
   ];
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels: Array<any> = ['$10', '$12', '$14', '$16', '$18', '$20', '$25'];
   public lineChartOptions: any = {
     responsive: true
   };
