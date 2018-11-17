c378083127e67909d1c6fed6cc9b7bef79d7b690
Johnny Pedregon <jonathanpedregon@gmail.com>
2018-11-17 15:56:34 -0700

    Trying to get charts to work

--- FrontEnd/src/app/buying/item-history/item-history.component.ts
+++ FrontEnd/src/app/buying/item-history/item-history.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
+import { ItemHistory } from 'src/app/models/item-history';
 
 @Component({
   selector: 'app-item-history',
   templateUrl: './item-history.component.html',
   styleUrls: ['./item-history.component.css']
 })
-export class ItemHistoryComponent implements OnInit {
+export class ItemHistoryComponent {
+  public lineChartData = [9.99, 10.41];
+  public lineChartLabels = ['09/01/2018', '10/01/2018'];
+  public lineChartType = 'line';
+
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
