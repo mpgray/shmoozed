46aad43a203434b039d508d2d1813ce1fea73009
Johnny Pedregon <jonathanpedregon@gmail.com>
2018-11-18 19:50:16 -0700

    Finalizing item history page.

--- FrontEnd/src/app/buying/item-history/item-history.component.ts
+++ FrontEnd/src/app/buying/item-history/item-history.component.ts
@@ -8,9 +8,16 @@ import { ItemHistory } from 'src/app/models/item-history';
   styleUrls: ['./item-history.component.css']
 })
 export class ItemHistoryComponent {
-  public lineChartData = [9.99, 10.41];
-  public lineChartLabels = ['09/01/2018', '10/01/2018'];
-  public lineChartType = 'line';
+  chartOptions = {
+    responsive: true
+  };
+
+  chartData = [
+    { data: [10.51, 13.34, 15.41, 14.87, 15.51, 13.74, 15.91, 16.87], label: 'Seller 1' },
+    { data: [10.99, 12.24, 14.35, 13.29, 13.51, 12.34, 16.41, 13.87], label: 'Seller 2' }
+  ];
+
+  chartLabels = ['9/1/2018', '9/15/2018', '10/1/2018', '10/15/2018', '11/1/2018', '11/15/2018', '12/1/2018', '12/15/2018'];
 
 
   constructor() { }
