4fc3eb01078876a5d113e98d9677645b9f0db131
Daniel <dkiser8@gmail.com>
2018-11-28 23:00:24 -0700

    Add SellerInsightControllerTest
    Controller tested for JSON output looks good.

--- BackEnd/shmoozed/src/test/java/com/shmoozed/controller/SellerInsightControllerTest.java
+++ BackEnd/shmoozed/src/test/java/com/shmoozed/controller/SellerInsightControllerTest.java
@@ -1,5 +1,48 @@
 package com.shmoozed.controller;
 
+import com.shmoozed.model.DemandPricevsRevenueDataPoint;
+import com.shmoozed.repository.BuyerItemRepository;
+import com.shmoozed.service.SellerInsightService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static java.util.Arrays.asList;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SellerInsightControllerTest {
 
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private SellerInsightService mockSellerInsightService;
+
+  @MockBean
+  private BuyerItemRepository mockBuyerItemRepository;
+
+  @Test
+  public void getPricevsRevenue() throws Exception {
+    DemandPricevsRevenueDataPoint dp001 = new DemandPricevsRevenueDataPoint(1,61);
+    DemandPricevsRevenueDataPoint dp002 = new DemandPricevsRevenueDataPoint(2,112);
+
+    when(mockSellerInsightService.getAllRevenueByBuyerItemId(1)).thenReturn(asList(dp001, dp002));
+
+    mockMvc.perform(get("/sellerinsight/1"))
+      .andDo(print())
+      .andExpect(status().isOk())
+      .andExpect(content().json("[{\"demandPrice\":1.0,\"revenue\":61.0}," +
+                                          "{\"demandPrice\":2.0,\"revenue\":112.0}]"));
+  }
 }
