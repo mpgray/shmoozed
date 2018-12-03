dd6e56446d03dcef02e4147733d7da33444f072c
Johnny Pedregon <jonathanpedregon@gmail.com>
2018-12-03 12:40:16 -0700

    Changing login page to automatically go to dashboard when login button is clicked.

--- FrontEnd/src/app/account/login/login.component.ts
+++ FrontEnd/src/app/account/login/login.component.ts
@@ -9,23 +9,10 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 })
 export class LoginComponent {
 
-  constructor(
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+  constructor() {
   }
 
-  onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    const profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    const id_token = googleUser.getAuthResponse().id_token;
-    console.log('ID Token: ' + id_token);
+  onSignIn() {
   }
 
 }
