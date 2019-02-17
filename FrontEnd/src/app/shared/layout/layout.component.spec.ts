ea8087288b4d3cac3c5d9148a6a1ee51c1a84de0
Charles Durfee <charlesdurfee@gmail.com>
2019-02-17 14:28:01 -0700

    fixed layout component test

--- FrontEnd/src/app/shared/layout/layout.component.spec.ts
+++ FrontEnd/src/app/shared/layout/layout.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatDialogModule, MatDialog } from "@angular/material/dialog";
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,7 +14,10 @@ describe('LayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+      imports: [FormsModule, ReactiveFormsModule, MatAutocompleteModule,
+        AngularFontAwesomeModule, HttpClientTestingModule, MatDialogModule],
+      declarations: [ LayoutComponent ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
