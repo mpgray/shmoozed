9e251aa274d987a0ba4ddd0c2f2b2b8e8d0e6d61
Charles Durfee <charlesdurfee@gmail.com>
2019-02-17 13:57:33 -0700

    fixed chart component test

--- FrontEnd/src/app/shared/chart/chart.component.spec.ts
+++ FrontEnd/src/app/shared/chart/chart.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChartComponent } from './chart.component';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 describe('ChartComponent', () => {
   let component: ChartComponent;
@@ -8,6 +9,7 @@ describe('ChartComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ChartsModule],
       declarations: [ ChartComponent ]
     })
     .compileComponents();
