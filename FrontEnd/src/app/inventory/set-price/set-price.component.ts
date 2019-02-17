21f932161e77cbca57f094ced394d9344fa8bb3f
Charles Durfee <charlesdurfee@gmail.com>
2019-02-16 17:16:46 -0700

    tool now shows current cost and price values for the sellers items

--- FrontEnd/src/app/inventory/set-price/set-price.component.ts
+++ FrontEnd/src/app/inventory/set-price/set-price.component.ts
@@ -11,16 +11,18 @@ export class SetPriceComponent implements OnInit {
   @Input() itemId: number;
   // TODO use a real userID once authentication is setup
   @Input() setPriceTargetData = {itemId: this.itemId, price: 1, userId: 2, sellerCost: 1};
+  products: any = [];
+  isSellerItem: boolean;
 
   constructor(public rest: RESTService) { }
 
   ngOnInit() {
-
+    this.getItemData();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.itemId) {
-      
+      this.getItemData();
     }
   }
 
@@ -29,10 +31,28 @@ export class SetPriceComponent implements OnInit {
       this.setPriceTargetData.itemId = this.itemId;
       this.rest.updateSellerItem(this.itemId, this.setPriceTargetData).subscribe((result) => {
         // location.reload();
+        this.getItemData();
       }, (err) => {
         console.log(err);
       });
     }
   }
 
+  getItemData(){
+    this.products = [];
+    this.setPriceTargetData.price = null;
+    this.setPriceTargetData.sellerCost = null;
+    this.isSellerItem = false;
+    this.rest.getSellerItems().subscribe((data: {}) => {
+      this.products = data;
+      //console.log(data);
+      for (const product of this.products) {
+        if (product.itemId == this.itemId) {
+          this.setPriceTargetData.price = product.price;
+          this.setPriceTargetData.sellerCost = product.sellerCost;
+          this.isSellerItem = true;
+        }
+      }
+    });
+  }
 }
