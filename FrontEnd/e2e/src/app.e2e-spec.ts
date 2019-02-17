aa6cd63779642c16f4fdff428d25228bc1444cfa
Charles Durfee <charlesdurfee@gmail.com>
2019-02-17 11:27:46 -0700

    fixed tests for app component

--- FrontEnd/e2e/src/app.e2e-spec.ts
+++ FrontEnd/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to shmoozed!');
+    expect(page.getParagraphText()).toEqual('Buyers and sellers BOTH win!');
   });
 });
