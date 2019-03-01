6bf58635adb6b0d774950f8675c1aa67617155d2
Charles Durfee <charlesdurfee@gmail.com>
2019-03-01 11:06:44 -0700

    fixed failing test in table component

--- FrontEnd/src/app/shared/table/table.component.spec.ts
+++ FrontEnd/src/app/shared/table/table.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TableComponent } from './table.component';
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('TableComponent', () => {
   let component: TableComponent;
@@ -8,6 +10,7 @@ describe('TableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [NgxDatatableModule, HttpClientTestingModule],
       declarations: [ TableComponent ]
     })
     .compileComponents();
