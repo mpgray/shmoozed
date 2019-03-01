43598761f09281e73d8f00acd851fa5daa5872aa
Charles Durfee <charlesdurfee@gmail.com>
2019-03-01 09:35:31 -0700

    fixed failing onboard component test

--- FrontEnd/src/app/account/onboard/onboard.component.spec.ts
+++ FrontEnd/src/app/account/onboard/onboard.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OnboardComponent } from './onboard.component';
+import { AccountComponent} from "../account.component";
+import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
 
 describe('OnboardComponent', () => {
   let component: OnboardComponent;
@@ -8,7 +10,8 @@ describe('OnboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OnboardComponent ]
+      declarations: [ OnboardComponent, AccountComponent],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     })
     .compileComponents();
   }));
