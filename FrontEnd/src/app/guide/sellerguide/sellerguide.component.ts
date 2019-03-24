2c081b8d3b50b93bf722c0ad1ad3dae9b8cbf520
Charles Durfee <charlesdurfee@gmail.com>
2019-03-24 06:52:34 -0600

    added intro.js and initial guide for buyer and seller

--- FrontEnd/src/app/guide/sellerguide/sellerguide.component.ts
+++ FrontEnd/src/app/guide/sellerguide/sellerguide.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, SimpleChanges} from '@angular/core';
+import * as introJs from 'intro.js/intro.js';
 
 @Component({
   selector: 'app-sellerguide',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerguideComponent implements OnInit {
 
+  intro = introJs();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  startIntro(){
+    let options = {
+
+      steps: [
+        {
+          element: document.querySelector('#sidebarToggle'),
+          intro: "Click the Menu icon to open the side menu (if closed), then click 'Selling'",
+          disableInteraction: true
+        },
+        {
+          element: document.querySelector('#sidebarToggle'),
+          intro: "Click 'Next' to go to the Selling page",
+          disableInteraction: true
+        },
+        {
+          element: document.querySelector('#sidebarToggle'),
+          intro: "Going to seller page"
+        }
+      ]
+    };
+    this.intro.setOptions(options);
+    this.intro.start().onchange(() =>{
+      if (this.intro._currentStep == "1") {
+        document.getElementById('sidebarToggle').click();
+      }else if (this.intro._currentStep == "2"){
+        this.changePage();
+      }
+    });
+
+  }
+
+  changePage() {
+    window.location.href = 'inventory?multipage=true';
+  }
 }
