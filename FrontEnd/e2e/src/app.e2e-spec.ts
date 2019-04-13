ecb1a9e1532b40c17d26a5e2a9df130f8e9285f0
Unknown <jonathanpedregon@gmail.com>
2019-04-12 19:26:35 -0600

    Adding registration tests

--- FrontEnd/e2e/src/app.e2e-spec.ts
+++ FrontEnd/e2e/src/app.e2e-spec.ts
@@ -29,3 +29,84 @@ describe('Login Page', () => {
     expect(value).toBe('29');
   });
 });
+
+describe('Registration page', () => {
+  it('should set correct user id in local storage', () => {
+    browser.get('http://localhost:4200/register');
+    browser.executeScript('window.localStorage.clear();');
+    element(by.id('buyerRadioButton')).click();
+
+    const date = new Date();
+    const username = 'testusername' + date.getTime();
+    const usernameInput = element(by.id('usernameInput'));
+    usernameInput.clear();
+    usernameInput.sendKeys(username);
+    element(by.id('registerButton')).click();
+    browser.sleep(1000);
+
+    const roles = browser.executeScript('return window.localStorage.getItem(\'roles\');');
+    expect(roles).toBeTruthy();
+    expect(roles).toContain('user_Id');
+    expect(roles).toContain('role');
+  });
+
+  it('should register a buyer with the correct role', () => {
+    browser.get('http://localhost:4200/register');
+    browser.executeScript('window.localStorage.clear();');
+    element(by.id('buyerRadioButton')).click();
+
+    const date = new Date();
+    const username = 'testbuyer' + date.getTime();
+    const usernameInput = element(by.id('usernameInput'));
+    usernameInput.clear();
+    usernameInput.sendKeys(username);
+    element(by.id('registerButton')).click();
+    browser.sleep(1000);
+
+    const roles = browser.executeScript('return window.localStorage.getItem(\'roles\');');
+    expect(roles).toBeTruthy();
+    expect(roles).toContain('user_Id');
+    expect(roles).toContain('role');
+    expect(roles).toContain('Buyer');
+  });
+
+  it('should register a seller with the correct role', () => {
+    browser.get('http://localhost:4200/register');
+    browser.executeScript('window.localStorage.clear();');
+    element(by.id('sellerRadioButton')).click();
+
+    const date = new Date();
+    const username = 'testseller' + date.getTime();
+    const usernameInput = element(by.id('usernameInput'));
+    usernameInput.clear();
+    usernameInput.sendKeys(username);
+    element(by.id('registerButton')).click();
+    browser.sleep(1000);
+
+    const roles = browser.executeScript('return window.localStorage.getItem(\'roles\');');
+    expect(roles).toBeTruthy();
+    expect(roles).toContain('user_Id');
+    expect(roles).toContain('role');
+    expect(roles).toContain('Seller');
+  });
+
+  it('should register an admin with the correct role', () => {
+    browser.get('http://localhost:4200/register');
+    browser.executeScript('window.localStorage.clear();');
+    element(by.id('bothRadioButton')).click();
+
+    const date = new Date();
+    const username = 'testAdmin' + date.getTime();
+    const usernameInput = element(by.id('usernameInput'));
+    usernameInput.clear();
+    usernameInput.sendKeys(username);
+    element(by.id('registerButton')).click();
+    browser.sleep(1000);
+
+    const roles = browser.executeScript('return window.localStorage.getItem(\'roles\');');
+    expect(roles).toBeTruthy();
+    expect(roles).toContain('user_Id');
+    expect(roles).toContain('role');
+    expect(roles).toContain('Admin');
+  });
+});
