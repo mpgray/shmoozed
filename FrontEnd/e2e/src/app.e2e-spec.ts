368a7d301053b3f90c8a8daca9b83cf550272aff
Unknown <jonathanpedregon@gmail.com>
2019-04-12 18:43:30 -0600

    Adding login test

--- FrontEnd/e2e/src/app.e2e-spec.ts
+++ FrontEnd/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -10,5 +11,21 @@ describe('workspace-project App', () => {
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Buyers and sellers BOTH win!');
+  });
+});
+
+describe('Login Page', () => {
+  it('should set user data in local storage', () => {
+    browser.get('http://localhost:4200/onboard');
+    browser.executeScript('window.localStorage.clear();');
+    const username = 'oneworkingexample';
+    element(by.id('usernameInput')).clear();
+    element(by.id('usernameInput')).sendKeys(username);
+    const userButton = element(by.id('loginButton'));
+    userButton.click();
+    browser.sleep(1000);
+
+    const value = browser.executeScript('return window.localStorage.getItem(\'userId\');');
+    expect(value).toBe('29');
   });
 });
